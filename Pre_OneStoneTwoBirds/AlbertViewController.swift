5cd74ad16fc6f24b4fba458dc60f7c301f625dff
kyurorong <zzikcomputer@gmail.com>
2024-05-31 17:54:47 +0900

    소개화면 변경

--- Pre_OneStoneTwoBirds/AlbertViewController.swift
+++ Pre_OneStoneTwoBirds/AlbertViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 class AlbertViewController: UIViewController {
 
     @IBOutlet weak var AlbertTableView: UITableView!
+    let teamNames: [String] = ["김동언", "김인규", "송인우", "차해라"]
+    let MBTI: [String] = ["ENFP", "ENTP", "ESTP", "INTP"]
     
-    let TeamNames: [String] = ["김동건", "김인규", "송인우", "차해라"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,15 +25,15 @@ class AlbertViewController: UIViewController {
 
 extension AlbertViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return TeamNames.count
+        return teamNames.count //데이터 몇 개?
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell =
-        AlbertTableView
-            .dequeueReusableCell(withIdentifier: "status", for: indexPath)
-        cell.textLabel?.text = TeamNames[indexPath.row]
-        return cell
+        let cell = AlbertTableView.dequeueReusableCell(withIdentifier: "status1", for: indexPath) as! Type1
+        cell.LabelText.text = teamNames[indexPath.row]
+        //as? as! - 부모 자식 친자확인
+        return cell //무엇을 보여줄래? 반복?
+
     }
     
 
---
+++ Pre_OneStoneTwoBirds/Type1.swift
@@ -0,0 +1,18 @@
+//
+//  status1.swift
+//  Pre_OneStoneTwoBirds
+//
+//  Created by 김인규 on 5/31/24.
+//
+
+import UIKit
+
+class Type1 : UITableViewCell {
+    //label
+  
+    @IBOutlet weak var LabelText: UILabel!
+    
+    @IBOutlet weak var MBTIText: UILabel!
+    
+    
+}
