9d15fcc2cbe7fad26f855d4ccc42bcd51592a72d
Inu <siw3355@gmail.com>
2024-05-31 10:58:43 +0900

    Initial Commit

---
+++ Pre_OneStoneTwoBirds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pre_OneStoneTwoBirds
+//
+//  Created by t2023-m0119 on 5/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
